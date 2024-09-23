7d95430f0ea69bc6ec3541c84f6349c0e291d201
ornobaadi <aadi4789@gmail.com>
2024-09-24 04:54:46 +0600

    bug check

--- js/login.js
+++ js/login.js
@@ -16,7 +16,7 @@ document.getElementById('button-login')
         // Not perfect way
         if (phoneNumber === '5' && pinNumber === '1234') {
             console.log('You are logged in')
-            window.location.href = '/home.html'
+            window.location.href = 'home.html'
         }
         else {
             alert('Wrong Phone number or PIN');
